0b334cd013672e112bde8930debf9df691b102f7
kodaoluidris <kodaoluidris@gmail.com>
2024-12-15 22:43:52 +0100

    created a page to check backend working

--- src/server.js
+++ src/server.js
@@ -13,6 +13,8 @@ const app = express();
 app.use(helmet());
 app.use(cors({
   origin: process.env.CORS_ORIGIN || '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
   credentials: true
 }));
 
