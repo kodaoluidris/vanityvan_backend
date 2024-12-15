d6c4fbdbcf50319c7c6379f250b040cdfbfd33e7
kodaoluidris <kodaoluidris@gmail.com>
2024-12-15 18:25:08 +0100

    connect to all port

--- src/server.js
+++ src/server.js
@@ -28,7 +28,7 @@ app.use('/api', routes);
 app.use('/api/uploads', express.static(path.join(__dirname, '../uploads')));
 
 const PORT = process.env.PORT || 9000;
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
